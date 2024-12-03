f3e50097f501e44761eadc4019081c3c84f87625
Juanjo <jjplo2019@gmail.com>
2024-12-03 22:23:25 +0100

    Proyecto terminado

---
+++ src/navbar2.php
@@ -0,0 +1,30 @@
+<!-- <nav class="navbar2">
+    <ul>
+        <li>
+            <a href="home.php">
+                <span class="icon"><ion-icon name="home-outline"></ion-icon></span>
+            </a>
+        </li>
+        <li>
+            <a href="">
+                <span class="icon"><ion-icon name="chatbubble-outline"></ion-icon></span>
+            </a>
+        </li>
+        <li>
+            <a href="search.php">
+                <span class="icon"><ion-icon name="search-outline"></ion-icon></span>
+            </a>
+        </li>
+        <li class="active point">
+            <a href="profile.php">
+                <span class="icon"><ion-icon name="person-outline"></ion-icon></span>
+            </a>
+        </li>
+        <li>
+            <a href="logic/functions/logoutUser.php">
+                <span class="icon"><ion-icon name="log-out-outline"></ion-icon></span>
+            </a>
+        </li>
+        <div class="indicator2"><span></span></div>
+    </ul>
+</nav> -->
